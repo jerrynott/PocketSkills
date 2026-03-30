70b3c810b3d9c29bdc2fe6ed37a5706a1e5211c7
ckeviny <ckeviny@gmail.com>
2026-03-30 17:31:30 +1100

    Test - Only opens on search bar

--- PocketSkills/Scripts/library.js
+++ PocketSkills/Scripts/library.js
@@ -20,7 +20,6 @@ function Library(element, data) {
         if (!search) {
             _this.$list.find('.folder').addClass('open');
             _this.$list.find('.item').show();
-            _this.$list.find('.folder').removeClass('open');
         } else {
             _this.$list.find('.item').hide();
             _this.$list.find('.item').each(function () {
