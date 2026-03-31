457bd2a548dd34dfde8400102347cd7157e72751
ckeviny <ckeviny@gmail.com>
2026-03-31 16:00:29 +1100

    Changed click() to trigger('click') in folder

--- PocketSkills/Scripts/library.js
+++ PocketSkills/Scripts/library.js
@@ -20,10 +20,7 @@ function Library(element, data) {
         if (!search) {
             _this.$list.find('.folder').addClass('open');
             _this.$list.find('.item').show();
-            _this.$list.find('.folder').each(function () {
-                console.log(`Click list button here`)
-                $(this).click();
-            })
+            _this.$list.find('.folder').trigger("click")
         } else {
             _this.$list.find('.item').hide();
             _this.$list.find('.item').each(function () {
